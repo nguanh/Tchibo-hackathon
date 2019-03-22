5b67e8c891519a71b1464cae034eff524790f3c4
Anh Tuan <Nguyen@carekura.de>
2019-03-22 20:03:30 +0000

    StoreDatabase works

--- StoreDatabase.py
+++ StoreDatabase.py
@@ -2,7 +2,9 @@ from mysqlWrapper.mariadb import MariaDb
 from conf.config import get_config
 from HashTagExtractor import get_data
 
-ADD_HASHTAG = ("INSERT INTO hashtag (name) VALUES (%s)")
+ADD_HASHTAG = ("INSERT INTO hashtag (name) VALUES (%s) ON DUPLICATE KEY UPDATE name = VALUES(name)")
+ADD_PRODUCT = ("INSERT INTO product (title, img) VALUES (%s, %s)")
+ADD_PRODUCT_HASHTAG = ("INSERT INTO product_hashtag (product_id, hashtag_id) VALUES (%s, %s)")
 
 
 config = get_config("MARIADBX")
@@ -19,10 +21,27 @@ for value in hashtags:
         if len(token) == 0:
             continue
         unique_hashtags.add("#"+token)
-print(unique_hashtags)
 
-#TODO wtf
-counter = 1
+
 for uni in unique_hashtags:
-    print(uni)
-    connector.execute_ex(ADD_HASHTAG,(uni))
+    try:
+        connector.execute_ex(ADD_HASHTAG,(uni))
+        print("{} added".format(uni))
+    except e:
+        print("{} could not be added {}".format(uni, e))
+
+for index,row in df.iterrows():
+    data = (row["TITLE"] or "", row["MAIN_IMAGE_URL"] or "")
+    try:
+        print(data)
+        product_id = connector.execute_ex(ADD_PRODUCT, data)
+        print("{} product added".format(row["PRODUCT_ID"]))
+        for uni in unique_hashtags:
+            if uni in row["HASHTAGS"]:
+                try:
+                    connector.execute_ex(ADD_PRODUCT_HASHTAG, (product_id, uni))
+                except e:
+                    print(e)
+    except e:
+        print("{} product could not be added {}".format(row["TITLE"] or "",e))
+
